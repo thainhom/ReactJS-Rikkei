c2b6e54668a0bbd80303027f082642af56c3e86f
ThaiLQ5 <thainhom1206@gmail.com>
2023-05-31 19:43:10 +0700

    session01-bai-tap03

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    let datetime = new Date();
+    return (
+      <div>
+        <h1 className="bt3">Xin chao cac ban bay h la:</h1>
+        {datetime.toLocaleTimeString()}
+      </div>
+    );
+  }
 }
 
 export default App;
