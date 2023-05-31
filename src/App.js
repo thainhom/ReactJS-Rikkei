abd31fedc59af3e06fe7a1dbf961f2b0438fc96c
ThaiLQ5 <thainhom1206@gmail.com>
2023-05-31 19:22:38 +0700

    session01-bai-tap-02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  sum = (a, b) => {
+    return a + b;
+  };
+  render() {
+    let a = 3;
+    let b = 4;
+
+    return (
+      <div>
+        Tong cua {a} va {b} la {this.sum(a, b)}
+      </div>
+    );
+  }
 }
 
 export default App;
