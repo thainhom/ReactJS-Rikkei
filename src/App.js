1a33580a8633b259c99fa3cbda9c9200deb94e98
ThaiLQ5 <thainhom1206@gmail.com>
2023-06-02 05:41:08 +0700

    session02-tes1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
+import Parentercomponent from "./Parentercomponent";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { name: "vietnam" };
-  }
   render() {
+    user = {
+      users: [
+        { id: "user1", title: "op", salary: "300 $" },
+        { id: "user2", title: "RV", salary: "500 $" },
+        { id: "user3", title: "lead", salary: "2000 $" },
+      ],
+    };
     return (
       <div>
-        name
-        {this.state.name}
+        <Parentercomponent a={this.state.user} />
       </div>
     );
   }
 }
+
 export default App;
