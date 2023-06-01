7c155bbb71e8092e77725bc8fd61fc40f71a542c
ThaiLQ5 <thainhom1206@gmail.com>
2023-06-02 05:54:00 +0700

    Revert "session02-tes1"
    This reverts commit 1a33580a8633b259c99fa3cbda9c9200deb94e98.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
-import Parentercomponent from "./Parentercomponent";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { name: "vietnam" };
+  }
   render() {
-    user = {
-      users: [
-        { id: "user1", title: "op", salary: "300 $" },
-        { id: "user2", title: "RV", salary: "500 $" },
-        { id: "user3", title: "lead", salary: "2000 $" },
-      ],
-    };
     return (
       <div>
-        <Parentercomponent a={this.state.user} />
+        name
+        {this.state.name}
       </div>
     );
   }
 }
-
 export default App;
