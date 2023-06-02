783b6eb6624777954bbc52e4376f2c24c6e9b7cd
ThaiLQ5 <thainhom1206@gmail.com>
2023-06-02 18:25:24 +0700

    revert master to init started app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from "react";
-import Parentercomponent from "./Parentercomponent";
 
 class App extends React.Component {
   render() {
-    user = {
-      users: [
-        { id: "user1", title: "op", salary: "300 $" },
-        { id: "user2", title: "RV", salary: "500 $" },
-        { id: "user3", title: "lead", salary: "2000 $" },
-      ],
-    };
     return (
       <div>
-        <Parentercomponent a={this.state.user} />
       </div>
     );
   }
