e22f511e32dcb1c9d6f0da2f3288bae9c12ccd8e
ThaiLQ5 <thainhom1206@gmail.com>
2023-06-03 19:48:59 +0700

    session03-bai-tap-dong-ho

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      time: new Date()
+    };
+  } 
+
+
+
+   componentDidMount(){
+    setInterval(this.chay,1000)
+  }
+     
+  
+  chay=()=>{
+    this.setState({
+      time: new Date()
+    })
+   
+  }
+  
   render() {
     return (
       <div>
+     <p>chao cac ban bay h la:{this.state.time.toLocaleDateString()}</p>
+         
       </div>
     );
   }
