2a85ee0f3a5643f20c357ad5818e380677873d0d
ThaiLQ5 <thainhom1206@gmail.com>
2023-06-06 22:02:36 +0700

    session04-thựchanh01

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
+
+function TaskList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const [error, setError] = useState(null);
+
+  function addTask() {
+    if (!tasks || !newTask) {
+      setError("vui long ko de trong")
+      return
+    } else {
+      setTasks([...tasks, newTask]);
+      setNewTask(
+
+      );
+    }
+
+
+
+
+
   }
-}
 
-export default App;
+
+  function handleChange(event) {
+    setNewTask(event.target.value);
+
+
+  }
+
+
+  return (
+    <div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+      <input type="text" value={newTask} onChange={handleChange} />
+      <button onClick={addTask}>Add Task</button>
+      {{ error } && <span>{error}</span>}
+    </div>
+  );
+}
+export default TaskList;
