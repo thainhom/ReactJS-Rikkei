cc82874b302fa28324620f8470dbd4631d07b3e1
ThaiLQ5 <thainhom1206@gmail.com>
2023-06-07 19:29:02 +0700

    session05-cai-dat-react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import "bootstrap/dist/css/bootstrap.css";
+// import Button from "react-bootstrap/Button"
+import { Button } from "react-bootstrap";
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Button>click</Button>
+        <button>click</button>
+
       </div>
     );
   }
