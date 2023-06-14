eb5e8cef8c41e7750682b5041d2ef9a55356b41a
ThaiLQ5 <thainhom1206@gmail.com>
2023-06-14 20:09:02 +0700

    session05-bai-tap

--- src/App.js
+++ src/App.js
@@ -10,10 +10,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { async } from "q";
 import { wait } from "@testing-library/user-event/dist/utils";
-
 // import { error } from "console";
 class App extends React.Component {
-
   constructor() {
     super();
     this.state = {
@@ -34,9 +32,7 @@ class App extends React.Component {
       // KHI BẤM SHOW SE SET LẠI ERROR KO BỊ DÍNH HIỆN THỊ ERROR
       error: ""
     });
-
   }
-
   // DÙNG async  await đê chờ set sử lý rồi log ra
   // ADD
   handleAdd = async () => {
@@ -46,10 +42,8 @@ class App extends React.Component {
         error: "Vui lòng điền công việc cần làm."
       });
     } else {
-
       const { todos, newtodos, index } = this.state;
       if (index >= 0) {
-
         // ĐANG TRONG QUÁ TRÌNH CHỈNH SỬA PHẦN TỬ ĐÓ (cu thể la làm tk newtodos)
         // CHO 1 BIẾN ĐÊ LẤY ĐƯỢC GIÁ TRỊ BAN ĐẦU ĐÊ CHỈNH SỬA
         const updatedTodos = [...todos];
@@ -75,9 +69,6 @@ class App extends React.Component {
       console.log(this.state.error, this.state.todos);
     }
   }
-
-
-
   handleChange = (event) => {
     // đặt biến đê lấy được giá trị ô input nhập vào
     const newtodos = event.target.value
@@ -86,16 +77,13 @@ class App extends React.Component {
   }
   // đê thực hiện việc chỉnh sửa và xóa cần phải trỏ tới vị trí của phần tử đó đê thao tác
   handleEditTodo = (index) => {
-
     const { todos } = this.state
     const edittodos = todos[index]
     this.setState({
-
       newtodos: edittodos,
       show: true,
       index: index,
     })
-
   }
   // 
   handleDeleteTodo = async (index) => {
@@ -103,33 +91,26 @@ class App extends React.Component {
       const { todos } = this.state
       const updatetodos = [...todos]
       // dung splice thực hiện đê xóa 1 phần tử trong mãng đả khởi tạo ở trên 
-
-
       updatetodos.splice(index, 1)// index chỉ mục phần tử muốn xóa tròng mãng , 1 là số chỉ định số lương muốn xóa
-
       await this.setState(
         {
-
           todos: updatetodos,
           totoDisplay: updatetodos,
         }
       )
     }
-
   }
   // Down là sự kiện khi nhấn 1 phím xún
   handleKeyDown = (event) => {
     if (event.key === "Enter") {
       event.preventDefault()
       this.handleAdd()
-
     }
   }
   // xử lý submit ko cho trang loading lại
   handleSubmit = (event) => {
     event.preventDefault()
   }
-
   handleSearch = async (event) => {
     await this.setState({
       filter: event.target.value
@@ -139,20 +120,13 @@ class App extends React.Component {
       totoDisplay: fiterdata
     })
   }
-
-
   render() {
     const { search, error, newtodos, todos, totoDisplay } = this.state
-
     return (
       <div>
-
-
-
         {/* navbar */}
         <Navbar bg="light" expand="lg">
           <Container fluid>
-
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
               <Nav
@@ -161,8 +135,6 @@ class App extends React.Component {
                 navbarScroll
               >
                 <Nav.Link href="#action1">Home</Nav.Link>
-
-
               </Nav>
               <Form className="d-flex">
                 <Form.Control
@@ -177,13 +149,7 @@ class App extends React.Component {
             </Navbar.Collapse>
           </Container>
         </Navbar>
-
         {/* <Button variant="outline-info">Info</Button> */}
-
-
-
-
-
         <h1 >List To Do</h1>
         <Button variant="outline-info" onClick={() => { this.handleShow() }}>
           Add
@@ -197,8 +163,9 @@ class App extends React.Component {
           >
             {/* modal , handleAdd */}
 
+
             <Modal.Header closeButton>
-              <Modal.Title>Thêm công việc cần làm</Modal.Title>
+              <Modal.Title >{this.state.todos.id ? "Sửa công việc" : "Thêm công việc cần làm "}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
               <Modal.Body>
@@ -272,5 +239,4 @@ class App extends React.Component {
     );
   }
 }
-
 export default App;
