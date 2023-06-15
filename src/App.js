2757f2d3ce68670a3d71f6f8b3e8cf222f88191f
ThaiLQ5 <thainhom1206@gmail.com>
2023-06-15 21:12:01 +0700

    session08-index

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
 import React from "react";
-import { Link, Route, Routes, } from "react-router-dom";
-import Home from "./components/Home"
-import About from "./components/About"
-import Product from "./components/Product"
-import AboutRikei from "./components/AboutRikei"
-import AboutRikeiS from "./components/AboutRikeiS"
-import { Outlet } from "react-router-dom";
+
 class App extends React.Component {
   render() {
     return (
-      <div className=" AppComponent">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/Product">Product</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          <Route path="/About" element={<About />} >
-            <Route path="AboutRikei" element={<AboutRikei />} />
-            <Route path="AboutRikeiS" element={<AboutRikeiS />} />
-          </Route>
-          <Route path="/Product" element={<Product />} />
-        </Routes>
+      <div>
       </div>
     );
   }
